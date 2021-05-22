6b914cdcda617ed7d9bced95ad8b18bb3f8bf67b
[kavinder panwar] <[9643kavinder@gmail.com]>
2021-05-22 17:38:24 +0530

    changes

--- deepdiagnosis/settings.py
+++ deepdiagnosis/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIS_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 CORS_ALLOW_ALL_ORIGINS = True
